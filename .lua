daf7d3f2edc9a4e0f8225f4edb039852c8041bcb
zyrask <kingnoobery@gmail.com>
2024-10-04 15:02:18 -0500

    Create .luae

---
+++ .lua
@@ -0,0 +1,10 @@
+local morons = {
+  "D15AD7A7-684F-4D82-99AF-87C1B42B3DFB",
+  "7b8bf85a-cf26-4f78-99e6-8a47f909625c",
+  "7aa91935-762d-40d9-9cfa-6564e6fcb946",
+  "e18ac2c1-d463-47f3-9222-8f6374fc8545",
+  "e5387aa3-4dc2-4508-90e8-59c947f9ff71",
+  "5287886d-e2a3-42f0-944b-8f66b6a5f613",
+  "0a5fe69c-5bd7-4ae6-914f-4927164c403a",
+}
+return morons
